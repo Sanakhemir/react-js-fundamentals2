a333669a0ffbf4491487df3444a07b745f716983
Sana Khemir <archi.khemirsana@gmail.com>
2024-12-25 17:55:33 +0100

    changement

---
+++ src/Player.js
@@ -0,0 +1,33 @@
+
+import React from "react";
+
+const Player = (Player) => {
+    const { name, team, nationality, jerseyNumber, age, imageUrl } = props;
+    
+    return (
+        <div>
+            <div>
+               <img src={imageUrl} alt={name}></img>
+            </div>
+            <div>
+               <h1>{name}</h1>
+            </div>
+
+            <div>
+               <h2>{team}</h2>
+               <h3>{nationality}</h3>
+               <h3>{jerseyNumber}</h3>
+               <h4>{age}</h4>
+            </div>
+
+        </div>
+
+      
+    )
+       
+}
+
+
+
+
+export default Player;
---
+++ src/PlayersList.js
@@ -0,0 +1,14 @@
+ import React from "react";
+ import players from "./players";
+ import Player from "./Player";
+
+
+ const PlayersList = (PlayersList) => {
+    // return (
+        
+    // ):
+ }
+
+
+
+export default PlayersList;
